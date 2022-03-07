cfe1029ce12152754420effd547ca49a332f2601
Roy Chua <rcdl2222@gmail.com>
2022-03-07 18:39:52 +0800

    Change description of fitness func

--- utils/fitness.py
+++ utils/fitness.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 def fitness(x):
-    """Define simple fitness function as
-    x^^2"""
+    """
+    Define simple fitness function as
+    (-(x-30)**2) + 100
+    """
     return (-(x-30)**2) + 100
